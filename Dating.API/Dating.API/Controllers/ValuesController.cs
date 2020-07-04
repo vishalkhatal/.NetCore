5aab322557c6d24f6a7ea0d85d693b81997623a0
Vishal Khatal <vishal.khatal2206@gmail.com>
2020-07-04 17:35:22 +0530

    Added initial skeleton

---
+++ Dating.API/Dating.API/Controllers/ValuesController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dating.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dating.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ValuesController(DataContext context)
+        {
+            this._context = context;
+        }
+        // GET api/values
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = _context.values.ToList();
+            return Ok(result);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            var result = _context.values.FirstOrDefault(x=>x.Id==id);
+            return Ok(result);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Dating.API/Dating.API/Model/Value.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dating.API
+{
+    public class Value
+    {
+        public decimal Id { get; set; }
+        public string Name { get; set; }
+    }
+}
